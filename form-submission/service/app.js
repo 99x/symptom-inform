5e3076909a1e3d0fb65bc06e694678d1f4e1172f
Ashan Fernando <Ashan Fernando>
2020-03-17 12:38:36 +0530

    Cleaning the form backend

--- form-submission/service/app.js
+++ form-submission/service/app.js
@@ -1,47 +1,31 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
 const AWS = require('aws-sdk');
 const uuid = require('uuid');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.TABLE_NAME;
 
-let response;
+exports.lambdaHandler = async (event) => {
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
-exports.lambdaHandler = async (event, context, callback) => {
-    console.log(event);
-    console.log(event.body);
     try {
+
         var params = {
             TableName: tableName,
             Item: {
                 Id: uuid.v4(),
                 CreatedAt: (new Date().getTime()).toString(),
-                Symptoms: event.body
+                Symptoms: JSON.parse(event.body)
             }
         }
 
-        const data = await dynamoDb.put(params).promise();
+        await dynamoDb.put(params).promise();
+
         return {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'Form submission succesfull! TableName:' + tableName,
-                data: data
+                message: 'Form submission succesfull!'
             })
-        }
+        };
+
     } catch (err) {
-        console.log(err);
         return err;
     }
 };
